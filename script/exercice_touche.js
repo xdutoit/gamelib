c983d4020ed7289783ca83c63d236808a79c65de
xdutoit <67094208+xdutoit@users.noreply.github.com>
2021-05-16 08:31:59 +0200

    0.1.4
    changement du texte, corrigé de l'exercice 1

--- script/exercice_touche.js
+++ script/exercice_touche.js
@@ -1,39 +1,39 @@
 function init(){
-	// inclure ici les images et mettre en place les éléments
-	GL.inclure('elem1','rect1');
+  // inclure ici les images et mettre en place les éléments
+  GL.inclure('elem1','rect1');
 
-	// placer à (0,0)
-	GL.setXY('elem1',0,0);
+  // placer à (0,0)
+  GL.setXY('elem1',0,0);
 
-	// dessiner
-	GL.dessiner('elem1');
+  // dessiner
+  GL.dessiner('elem1');
 
-	// déterminer la fonction à appeler lorsqu'une touche est enfoncée
-	GL.definirFonctionToucheEnfoncee(toucheAppuyee);
+  // déterminer la fonction à appeler lorsqu'une touche est enfoncée
+  GL.definirFonctionToucheEnfoncee(toucheAppuyee);
 }
 
 function toucheAppuyee(id_touche){
-	// 'id_touche' vaut le nom de la touche
+  // 'id_touche' vaut le nom de la touche
 
-	// flèche droite: on déplace l'élément de 10px dans la direction positive de l'axe x
-	if(id_touche=='ArrowRight'){
-		GL.addX('elem1',10);
-	}
-	// flèche gauche: on déplace l'élément de 10px dans la direction négative de l'axe x
-	if(id_touche=='ArrowLeft'){
-		GL.addX('elem1',-10);
-	}
+  // flèche droite: on déplace l'élément de 10px dans la direction positive de l'axe x
+  if(id_touche=='ArrowRight'){
+    GL.addX('elem1',10);
+  }
+  // flèche gauche: on déplace l'élément de 10px dans la direction négative de l'axe x
+  if(id_touche=='ArrowLeft'){
+    GL.addX('elem1',-10);
+  }
 
-	// flèche haut: on déplace l'élément de 10px dans la direction négative de l'axe y
-	if(id_touche=='ArrowUp'){
-		GL.addY('elem1',-10);
-	}
-	// flèche bas: on déplace l'élément de 10px dans la direction positive de l'axe x
-	if(id_touche=='ArrowDown'){
-		GL.addY('elem1',10);
-	}
+  // flèche haut: on déplace l'élément de 10px dans la direction négative de l'axe y
+  if(id_touche=='ArrowUp'){
+    GL.addY('elem1',-10);
+  }
+  // flèche bas: on déplace l'élément de 10px dans la direction positive de l'axe x
+  if(id_touche=='ArrowDown'){
+    GL.addY('elem1',10);
+  }
 
-	// dans tous les cas, on efface le canveas et on redessine l'lément à sa nouvelle position
-	GL.effacerTout();
-	GL.dessiner('elem1');
+  // dans tous les cas, on efface le canveas et on redessine l'lément à sa nouvelle position
+  GL.effacerTout();
+  GL.dessiner('elem1');
 }
